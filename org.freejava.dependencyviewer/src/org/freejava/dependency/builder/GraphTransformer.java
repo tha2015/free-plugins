1b26f87b1b775ba0082250a2aaa3c956adf11872
tha2015 <tha2015@users.noreply.github.com>
2017-04-09 20:22:14 -0700

    refactoring dependency viewer plugin

--- org.freejava.dependencyviewer/src/org/freejava/dependency/builder/GraphTransformer.java
+++
@@ -1,9 +0,0 @@
-package org.freejava.dependency.builder;
-
-import org.freejava.dependency.graph.Graph;
-
-public interface GraphTransformer<T> {
-
-    Graph<T> transform(Graph<T> graph);
-
-}
--- org.freejava.dependencyviewer/src/org/freejava/dependency/parser/ClassParser.java
+++
@@ -1,7 +0,0 @@
-package org.freejava.dependency.parser;
-
-import java.io.InputStream;
-
-public interface ClassParser {
-    ClassInfo parse(InputStream classFile) throws Exception;
-}
